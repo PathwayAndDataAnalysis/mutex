52d1cd18697807e463ce99daee8ecb7a0a41b3b2
DujianDing <dujiand@sfu.ca>
2018-06-26 20:22:19 -0700

    Update: constrain searched gene seeds in a random run by network;

--- src/main/java/org/cbio/mutex/Main.java
+++ src/main/java/org/cbio/mutex/Main.java
@@ -184,6 +184,7 @@ public class Main
 
 		MutexGreedySearcher searcher = new MutexGreedySearcher(genesMap, network);
 		Set<String> symbols = genesMap.keySet();
+		if (network != null) symbols.retainAll(network.getSymbols());
 		Set<String> noShuffle = loadHighlySignificantGenes();
 		generateRandomPvals(searcher, symbols, noShuffle, null, howMany);
 	}
